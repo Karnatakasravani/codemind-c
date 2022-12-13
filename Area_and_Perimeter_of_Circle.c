b77b30d4eff2bf0e976b80a6e55646b9f7e8047c
Karnatakasravani <119839721+Karnatakasravani@users.noreply.github.com>
2022-12-13 20:07:20 +0530

    #include<stdio.h> int main() {     int r;     scanf("%d",&r);     float area;     area=3.14*r*r;     printf("%0.2f ",area);     float perimeter;     perimeter=2*3.14*r;     printf("%0.2f ",perimeter); }

---
+++ Area_and_Perimeter_of_Circle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int r;
+    scanf("%d",&r);
+    float area;
+    area=3.14*r*r;
+    printf("%0.2f
+",area);
+    float perimeter;
+    perimeter=2*3.14*r;
+    printf("%0.2f
+",perimeter);
+}
